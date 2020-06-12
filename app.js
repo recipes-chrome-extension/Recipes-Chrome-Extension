301ad9e00e0c1857c86fa7665a9f248c74665aeb
Brandon Ellis <brandonellis.email@gmail.com>
2020-06-11 20:20:05 -0600

    init

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
-      console.log("The color is green.");
-    });
+var ready = (callback) => {
+    if (document.readyState != "loading") callback();
+    else document.addEventListener("DOMContentLoaded", callback);
+  }
+  
+  ready(() => { 
+    console.log('extension loaded')
   });
